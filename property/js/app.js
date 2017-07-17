e91d7ecec0f462bc4ef692ab3fce0f52512ba2f4
sc-aug <shichuanwang@yahoo.com>
2017-07-17 03:07:06 -0500

    basic

--- property/js/app.js
+++ property/js/app.js
@@ -1,122 +1,157 @@
 $(document).ready(function(){
-    // init
-    Controller.welcomePage();
-
-    $("#home").click(Controller.welcomePage);
-    $("#all-prob").click(Controller.allProblemPage);
+    // init page
+    initPage();
 });
 
 
-/********* Model *******************/
-var Model = {
-};
+function initPage() {
+    // init NavBar
+    initNavBar();
+    // init click handler
+    initHandler();
+}
 
-/********* Controller **************/
-var Controller = {
-    welcomePage: function() {
-        View.welcomePage(welcome_page);
-    },
+// init NavBar
+function initNavBar() {
+    $.ajax({ 
+        type: 'GET',
+        url: 'property/data/map_category_tag.json',
+        dataType: 'json',
+        success: function (data) {
+            $.each(data, function(i, obj) {
+                var tmp = $('<div>', { 'class': 'category_wrapper' });
+                var head = $('<div>', { 'class': 'nav-sidebar category-title' });
+                var list = $('<ul>', { 'id': obj.category_id, 'class': 'nav nav-sidebar collapse' });
 
-    allProblemPage: function() {
-        // Model get all data
-        View.allProblemPage(prob_data);
-    }
-};
-
-/********* View ********************/
-var View = {
-    welcomePage: function() {
-        View.toggle(50);
-        View.updateWelcomePage();
-        View.toggle(100);
-    },
-
-    allProblemPage: function(all_prob) {
-        View.toggle(50);
-        View.prepareContent(all_prob_wrapper);
-        View.appendAllProbTableRow(all_prob);
-        View.toggle(100);
-    },
-
-    toggle: function(time) {
-        $("#dynamic-page").toggle(time);
-    },
-
-    updateWelcomePage: function() {
-        View.changeContentHTML(welcome_page);
-    },
-
-    prepareContent: function(wrapper) {
-        var content = $("#dynamic-page").html(wrapper);
-    },
-
-    appendAllProbTableRow: function(prob) {
-        // for (var i = all_prob.length-1; i >= 0; i ++) {
-        for (var i = prob.length-1; i >= 0; i --) {
-            var p = prob[i];
-            var tr_obj = $("<tr>");
-            var td_obj;
-            // id
-            td_obj = $("<td>");
-            td_obj.html(p.stat.question_id);
-            tr_obj.append(td_obj);
-            // title
-            td_obj = $("<td>");
-            a_obj = $("<a>");
-            a_obj.attr("href", "https://leetcode.com/problems/" + p.stat.question__title_slug);
-            a_obj.attr("target", "_blank");
-            a_obj.html(p.stat.question__title);
-            td_obj.append(a_obj);
-            tr_obj.append(td_obj);
-            // diff
-            td_obj = $("<td>");
-            td_obj.html(p.difficulty.level);
-            tr_obj.append(td_obj);
-            // freq
-            td_obj = $("<td>");
-            td_obj.html(p.frequency.toFixed(0));
-            tr_obj.append(td_obj);
-            // prog
-            td_obj = $("<td>");
-            td_obj.html(p.progress.toFixed(0));
-            tr_obj.append(td_obj);
-
-            $("#t-body").append(tr_obj);
+                $("#nav-sidebar").append(tmp);
+                tmp.append(head);
+                tmp.append(list);
+                
+                // head 
+                head.append($('<a>', {
+                    "data-toggle": "collapse",
+                    "href": "#" + obj.category_id,
+                    "text": obj.category_name
+                }));
+                
+                $(obj.tags).each(function(index, item) {
+                    list.append($('<li>').append($('<a>', {'id': item.tag_id, 'text': item.tag_name})));
+                });
+            });
         }
-    },
+    });
+}
 
-    changeContentHTML: function(content) {
-        $("#dynamic-page").html(content);
+// init click Handler
+function initHandler() {
+    $.ajax({ 
+        type: 'GET',
+        url: 'property/data/map_tag_content.json',
+        dataType: 'json',
+        success: function(data) {
+            $.each(data, function(i, obj) {
+                console.log(obj.id);
+                $('#'+obj.id).click(clickHandler);
+            });
+        }
+    });
+}
+
+function clickHandler(obj) {
+    updatePage(obj.target.id);
+    //updatePage($(obj).attr('id'));
+}
+
+function updatePage(id) {// clean
+    cleanDynamicPage();
+
+    // add content
+    if (id == 'home') { // welcome page
+        welcome(getItem(id));
+    } else { // problem page
+        problems(getItem(id));
     }
-};
-/*
-var all_prob_wrapper = "<h4 class=\"sub-header\">Section title</h4>\
-          <div class=\"table-responsive\">\
-            <table class=\"table table-stripe\">\
-              <thead id=\"t-head\">\
-                <tr>\
-                  <th>No.</th>\
-                  <th>Title</th>\
-                  <th>Difficulty</th>\
-                  <th>Frequency</th>\
-                  <th>Progress</th>\
-                </tr>\
-              </thead>\
-              <tbody id=\"t-body\">\
-                <tr>\
-                  <td>1</td>\
-                  <td><a href=\"https://leetcode.com/problems/two-sum/\" target=\"_blank\">Two Sum</a></td>\
-                  <td>Hard</td>\
-                  <td>36.1</td>\
-                  <td>23.1</td>\
-                </tr>\
-                <tr>\
-                  <td>1,002</td>\
-                  <td>amet</td>\
-                  <td>consectetur</td>\
-                  <td>adipiscing</td>\
-                  <td>elit</td>\
-                </tr>\
-              </tbody>\
-            </table>\
-          </div>";*/
+}
+
+// welcome page content 
+function welcome(obj) {
+    $('#dynamic-page').html(obj.content);
+}
+
+// problem list page content
+function problems(obj) {
+    var header = $('<h4>', {'class': 'sub-header', 'text': obj.name });
+    var table_wrapper = $('<div>', {'class': 'table-responsive'});
+    var table = $('<table>', {'class': 'table table-stripe'});
+    var thead = $('<thead>');
+    var tbody = $('<tbody>');
+
+    $('#dynamic-page').append(header);
+    $('#dynamic-page').append(table_wrapper);
+    table_wrapper.append(table);
+    table.append(thead);
+    table.append(tbody);
+
+    // head
+    $(thead).append($('<th>', {'text': 'No.'}));
+    $(thead).append($('<th>', {'text': 'Title.'}));
+    $(thead).append($('<th>', {'text': 'Diff.'}));
+    $(thead).append($('<th>', {'text': 'Freq.'}));
+
+    // body
+    var prob_ids = obj.content;
+
+    $.ajax({
+        type: 'GET',
+        url: 'property/data/prob_data.json',
+        dataType: 'json',
+        success: function (data) {
+            $(data).each(function(index, prob) {
+                if (prob_ids.indexOf(prob.stat.question_id) >= 0) {
+                    var tr = $('<tr>');
+                    tbody.append(tr);
+                    
+                    tr.append($('<td>', {
+                        'text': prob.stat.question_id}
+                    ));
+                    tr.append($('<td>').append($('<a>', {
+                        'text': prob.stat.question__title,
+                        'target': '_blank',
+                        'href': 'https://leetcode.com/problems/' + prob.stat.question__title_slug
+                    })));
+                    tr.append($('<td>', {
+                        'text': prob.difficulty.level
+                    }));
+                    tr.append($('<td>', {
+                        'text': prob.frequency.toFixed(0)
+                    }));
+                }
+            });
+        }
+    });
+}
+
+// get item by category id (e.g. 'home', 'array', 'bs', 'dp', 'bst' ...)
+function getItem(id) {
+    var item = null;
+    $.ajax({ 
+        type: 'GET',
+        async:false,
+        url: 'property/data/map_tag_content.json',
+        dataType: 'json',
+        success: function (data) {
+            $.each(data, function(i, obj) {
+                if (obj.id == id) {
+                    item = obj;
+                    return false;
+                }
+            });
+        }
+    });
+    return item;
+}
+
+// clean page
+function cleanDynamicPage() {
+    $('#dynamic-page').empty();
+}
